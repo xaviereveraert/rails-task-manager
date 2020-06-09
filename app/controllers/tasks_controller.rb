1dc969fbb8deb4a9c93828b4814904ed73512232
Xavier Everaert <xeveraert@gmail.com>
2020-06-09 17:00:12 +0200

    tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+  @task = Task.new(tasks_params)
+    @task.save
+  end
+
+private
+
+def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+# get '/tasks', to: 'restaurants#index', as: :restaurants
+
+
+# verb 'path', to: 'controller#action'
+get '/tasks', to: 'tasks#index'
+
+
+
+# # CREATE A TASK
+  # # DISPLAYING THE FORM
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # # POST INFORMATION INTO DB
+  # post '/restaurants', to: 'restaurants#create'
+
+get '/tasks/new', to: 'tasks#new'
+
+post '/tasks', to: 'tasks#create'
+
+
+
+
+
+
+get '/tasks/:id', to: 'tasks#show', as: :task
+
+
+
+
 end
